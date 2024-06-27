3b356994f6d6cfa46f2da1a2d599a77d79258d65
Adam Silverstein <adamjs@google.com>
2024-06-27 10:13:38 +0200

    initial commit

---
+++ includes/class-performance-wizard-data-source-base.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * A base class for performsnce wizard data sources.
+ * 
+ * Each data source has attributes that describe the source, a method to get the data
+ * and a prompt to use when passing the data to the AI agent.
+ */
+
+class Performance_Wizard_Data_Source_Base {
+    // Properties
+
+    /**
+     * The name of the data source.
+     * 
+     * @var string
+     */
+    private $name;
+
+    /**
+     * The description of the data source.
+     * 
+     * @var string
+     */
+    private $description;
+
+    /**
+     * The prompt to use when passing the data to the AI agent.
+     * 
+     * @var string
+     */
+    private $prompt;
+
+    /**
+     * A plain text description of the shape of the data returned from the data source.
+     */
+    private $data_shape;
+
+    // Constructor
+
+    // Methods
+
+    /**
+     * Get the data from the data source.
+     * 
+     * @return mixed The data from the data source.
+     */
+    public function get_data() {
+        // To be implemented by subclasses.
+    }
+
+    /**
+     * Get the name of the data source.
+     * 
+     * @return string The name of the data source.
+     */
+    public function get_name() {
+        return $this->name;
+    }
+
+    /**
+     * Get the description of the data source.
+     * 
+     * @return string The description of the data source.
+     */
+    public function get_description() {
+        return $this->description;
+    }
+
+    /**
+     * Get the prompt to use when passing the data to the AI agent.
+     * 
+     * @return string The prompt to use when passing the data to the AI agent.
+     */
+    public function get_prompt() {
+        return $this->prompt;
+    }
+
+    /**
+     * Get the shape of the data returned from the data source.
+     */
+    public function get_data_shape() {
+        return $this->data_shape;
+    }
+}
---
+++ wp-performance-wizard.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: WP Performance Wizard.
+ * Plugin URI: https://github.com/adamsilverstein/wp-performance-wizard
+ * Description: A plugin that uses AI to help you optimize your WordPress site for performance.
+ * Version: 1.0.0
+ * Author: Adam Silverstein, Google
+ * Author URI: ttps://github.com/adamsilverstein
+ * License: GPLv2 or later
+ * Text Domain: wp-performance-wizard
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 6.0
+ * Requires PHP: 7.0 
+ * 
+ * @package wp-performance-wizard
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Load the plugin.
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-performance-wizard.php';
