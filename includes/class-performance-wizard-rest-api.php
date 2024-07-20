381dff0b31734b1efba3af25c63a789246d9592a
Adam Silverstein <adamjs@google.com>
2024-07-20 14:55:35 -0600

    Add a REST API class

---
+++ includes/class-performance-wizard-rest-api.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * A class to configure the Performance Wizard Rest API endpoint
+ */
+class Performance_Wizard_Rest_API {
+
+	/**
+	 * Keep a handle on the base wizard class.
+	 */
+	private $wizard;
+
+	/**
+	 * Construct the class, passed a reference to the base class.
+	 */
+	function __construct( $wizard ) {
+		$this->wizard = $wizard;
+	}
+
+	/**
+	 * Add a command endpoint for the performance wizard.
+	 * This endpoint will accept a command and return a response.
+	 *
+	 * @return void
+	 */
+	public function add_endpoint() {
+		add_action( 'rest_api_init', function () {
+			// Register the command route, requiring admin access to use.
+			register_rest_route( 'performance-wizard/v1', '/command/', array(
+				'methods'             => 'POST',
+				'callback'            => array( $this, 'handle_command' ),
+				'permission_callback' => function () {
+					return current_user_can( 'manage_options' );
+				},
+			) );
+		} );
+	}
+
+	/**
+	 * Handle a command sent to the Rest API 'command' endpoint.
+	 *
+	 * Takes commands including 'get_next_action', 'start' and
+	 * 'run_next_action' and returns a response.
+	 */
+	public function handle_command( $request ) {
+		$command = $request->get_param( 'command' );
+
+		switch ( $command ) {
+			case '_get_next_action_':
+				$response = $this->wizard->get_next_action();
+				break;
+			case '_start_':
+				$response = $this->wizard->start();
+				break;
+			case '_run_next_action_':
+				$response = $this->wizard->run_next_action();
+				break;
+			default:
+				$response = $this->wizard->prompt( $command );
+		}
+
+		return new WP_REST_Response( $response, 200 );
+	}
+
+}
