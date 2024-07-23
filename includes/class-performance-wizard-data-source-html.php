41599b4f448d3e63d4002c28a2dd080b78409464
Adam Silverstein <adamjs@google.com>
2024-07-23 12:18:04 -0600

    remove  ${website} references

--- includes/class-performance-wizard-data-source-html.php
+++ includes/class-performance-wizard-data-source-html.php
@@ -17,7 +17,7 @@
 	/**
 	 * The prompt to use when passing the data to the AI agent.
 	 */
-	private $prompt = "This data source provides the HTML data for the site. The URL of the site you are testing is ${website}.";
+	private $prompt = "This data source provides the HTML data for the site.";
 
 	/**
 	 * A plain text description of the shape of the data returned from the data source.
@@ -27,5 +27,5 @@
 	/**
 	 * The description of a strategy that can be used to analyze this data source.
 	 */
-	private $analysis_strategy = "The HTML data source can be analyzed by looking for common performance issues in the HTML. Particular attention can be paid to issues identified in the Lighthouse audit at the beginning of the analysis. Files loaded from a WordPress plugin or theme can typically be identified by their path: /wp-content/plugins/<plugin-slug> or /wp-content/themes/<theme-slug>. This analysis is for the website ${website}. so scripts from other domains (so called 'third party scripts') should be reviewed individually for their potential performance impact.";
+	private $analysis_strategy = "The HTML data source can be analyzed by looking for common performance issues in the HTML. Particular attention can be paid to issues identified in the Lighthouse audit at the beginning of the analysis. Files loaded from a WordPress plugin or theme can typically be identified by their path: /wp-content/plugins/<plugin-slug> or /wp-content/themes/<theme-slug>. This analysis is for the website so scripts from other domains (so called 'third party scripts') should be reviewed individually for their potential performance impact.";
  }
