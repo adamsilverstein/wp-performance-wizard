047aa48ea31d5c6f23ff5ac47be8bd64f8d59078
Adam Silverstein <adamjs@google.com>
2024-11-06 10:19:09 -0700

    Add key entry encryption, bump.

--- wp-performance-wizard.php
+++ wp-performance-wizard.php
@@ -3,14 +3,14 @@
  * Plugin Name: WP Performance Wizard
  * Plugin URI: https://github.com/adamsilverstein/wp-performance-wizard
  * Description: A plugin that uses AI to help you optimize your WordPress site for performance.
- * Version: 1.3.0
+ * Version: 1.3.1
  * Author: Adam Silverstein, Google
  * Author URI: https://github.com/adamsilverstein
  * License: GPLv2 or later
  * Text Domain: wp-performance-wizard
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Requires at least: 6.0
- * Requires PHP: 7.0
+ * Requires PHP: 7.4
  *
  * @package wp-performance-wizard
  */
